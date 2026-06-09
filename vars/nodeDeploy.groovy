aef9496e635389722342567fddb92b53cb504450
ajay-nimap <ajay.more@nimapinfotech.com>
2026-06-09 11:53:14 +0530

    shared library groovy added

---
+++ vars/nodeDeploy.groovy
@@ -0,0 +1,25 @@
+def call() {
+
+    def imageName = "node-app"
+
+    stage('Checkout') {
+        checkout scm
+    }
+
+    stage('Install Dependencies') {
+        sh 'npm install'
+    }
+
+    stage('Build Docker Image') {
+        sh 'docker build -t  $IMAGE_NAME .'
+    }
+
+    stage('Deploy') {
+        sh '''
+            docker compose down || true
+            docker compose up -d --build
+        '''
+    }
+
+  
+}
